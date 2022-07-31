df98659e6c9ee13dcf5843ce6a8019b511df8e6b
Que121 <1095076407@qq.com>
2022-07-31 18:33:27 +0800

    que

--- main.cpp
+++ main.cpp
@@ -1,39 +1,58 @@
 #include "main.hpp"
 
+cv::Mat src = imread(TEST_PATH, IMREAD_COLOR);
+cv::Mat grey_dst = Mat ::zeros(src.size(), src.type());
+cv::Mat morphologyEx_dst = Mat ::zeros(src.size(), src.type());
+cv::Mat threshold_dst = Mat ::zeros(src.size(), src.type());
+cv::Mat erode_dst1 = Mat ::zeros(src.size(), src.type());
+cv::Mat erode_dst2 = Mat ::zeros(src.size(), src.type());
+cv::Mat dilate_dst1 = Mat ::zeros(src.size(), src.type());
+cv::Mat dilate_dst2 = Mat ::zeros(src.size(), src.type());
+
+float alpha = 2.5;                // 对比度
+float beta = 170;                 // 亮度
+float g_nStructElementSize1 = 30; //结构元素(内核矩阵)的尺寸
+float g_nStructElementSize2 = 7.5;
+float g_nStructElementSize3 = 6;
+float g_nStructElementSize4 = 3;
+float g_nStructElementSize5 = 6;
+
+int g_nStructElementSize = 10;
+int thresholds = 150;
+
+cv::Mat element1 = getStructuringElement(MORPH_ELLIPSE,
+                                         Size(g_nStructElementSize1,
+                                              g_nStructElementSize1)); // 膨胀腐蚀内核
+cv::Mat element2 = getStructuringElement(MORPH_RECT,
+                                         Size(g_nStructElementSize2,
+                                              g_nStructElementSize2));
+cv::Mat element3 = getStructuringElement(MORPH_ELLIPSE,
+                                         Size(g_nStructElementSize3,
+                                              g_nStructElementSize3));
+cv::Mat element4 = getStructuringElement(MORPH_RECT,
+                                         Size(g_nStructElementSize4,
+                                              g_nStructElementSize4));
+
+// threshold阈值滑动条回调函数
+void threshold_track(int typeValue, void *)
+{
+  cv::threshold(grey_dst, threshold_dst, thresholds, 255, THRESH_TOZERO);
+  imshow("threshold_dst", threshold_dst);
+}
+
+// morphologyEx滑动条回调函数
+void morphologyEx_track(int typeValue, void *)
+{
+  cv::Mat element = getStructuringElement(MORPH_ELLIPSE,
+                                          Size(g_nStructElementSize,
+                                               g_nStructElementSize));
+  morphologyEx(threshold_dst, morphologyEx_dst, MORPH_CLOSE, element);
+  imshow("morphologyEx_dst", morphologyEx_dst);
+}
+
 int main()
 {
-  cv::Mat src = imread(TEST_PATH, IMREAD_COLOR);
-  cv::Mat grey_dst = Mat ::zeros(src.size(), src.type());
-  cv::Mat morphologyEx_dst = Mat ::zeros(src.size(), src.type());
-  cv::Mat threshold_dst = Mat ::zeros(src.size(), src.type());
-  cv::Mat erode_dst1 = Mat ::zeros(src.size(), src.type());
-  cv::Mat erode_dst2 = Mat ::zeros(src.size(), src.type());
-  cv::Mat dilate_dst1 = Mat ::zeros(src.size(), src.type());
-  cv::Mat dilate_dst2 = Mat ::zeros(src.size(), src.type());
-
-  float alpha = 2.5;                       // 对比度
-  float beta = 170;                        // 亮度
   cvtColor(src, grey_dst, COLOR_BGR2GRAY); // 灰度化
-  float g_nStructElementSize1 = 4;         //结构元素(内核矩阵)的尺寸
-  float g_nStructElementSize2 = 7.5;
-  float g_nStructElementSize3 = 6;
-  float g_nStructElementSize4 = 3;
-  float g_nStructElementSize5 = 6;
-  cv::Mat element1 = getStructuringElement(MORPH_ELLIPSE,
-                                           Size(g_nStructElementSize1,
-                                                g_nStructElementSize1)); // 膨胀腐蚀内核
-  cv::Mat element2 = getStructuringElement(MORPH_RECT,
-                                           Size(g_nStructElementSize2,
-                                                g_nStructElementSize2));
-  cv::Mat element3 = getStructuringElement(MORPH_ELLIPSE,
-                                           Size(g_nStructElementSize3,
-                                                g_nStructElementSize3));
-  cv::Mat element4 = getStructuringElement(MORPH_RECT,
-                                           Size(g_nStructElementSize4,
-                                                g_nStructElementSize4));
-  cv::Mat element5 = getStructuringElement(MORPH_ELLIPSE,
-                                           Size(g_nStructElementSize5,
-                                                g_nStructElementSize5));
 
   // 灰度图调整对比度和亮度
   if (grey_dst.channels() == 1)
@@ -53,13 +72,19 @@ int main()
     // imwrite(GREY_SAVE_PATH, grey_dst);
   }
 
-  cv::threshold(grey_dst, threshold_dst, 150, 255, THRESH_BINARY);
-  imshow("threshold_dst", threshold_dst);
+  namedWindow("threshold_dst", WINDOW_AUTOSIZE);
+  cv::createTrackbar("threshold阈值:", "threshold_dst", &thresholds, 255, threshold_track); // 创建threshold阈值滑动条 70
+  threshold_track(0, 0);
+  waitKey(0);
 
 #if IS_CLOSED
   // 闭运算
-  morphologyEx(grey_dst, morphologyEx_dst, MORPH_CLOSE, element1);
-  imshow("morphologyEx_dst", morphologyEx_dst);
+  // morphologyEx(threshold_dst, morphologyEx_dst, MORPH_CLOSE, element1);
+  // imshow("morphologyEx_dst", morphologyEx_dst);
+  namedWindow("morphologyEx_dst", WINDOW_AUTOSIZE);
+  cv::createTrackbar("morphologyEx阈值:","morphologyEx_dst", &g_nStructElementSize,50,morphologyEx_track); // 创建morphologyEx滑动条 14
+  morphologyEx_track(0, 0);
+
 #else
   // 腐蚀_1
   cv::erode(grey_dst, erode_dst1, element1);
@@ -87,10 +112,10 @@ int main()
 
 #endif
 
-  // namedWindow("Test window", WINDOW_AUTOSIZE);
-  // imshow("Test window", grey_dst);
+  namedWindow("Test window", WINDOW_AUTOSIZE);
+  imshow("Test window", grey_dst);
   // imwrite(SAVE_PATH, grey_dst);
   waitKey(0);
 
   return 0;
-}  
+}
